5a7ece0c0cb551f762f57a2e5eec2c697a755e35
Yutao Xu <yutao.xu@intel.com>
2024-10-10 16:13:10 +0800

    Update report.py

--- report.py
+++ report.py
@@ -38,6 +38,7 @@ def parse_keys(folder, backend):
         match = re.search(pattern, line)
         keys.append(match.group(1))
     assert len(lines) == len(keys)
+    keys = set(keys)
     return keys
 
 
@@ -48,25 +49,27 @@ if __name__ == '__main__':
     with open('ipex_functions.yaml', 'r', encoding='utf-8') as file:
         data = yaml.safe_load(file)
         data = data['supported']
-    ipex_keys = data
+    ipex_keys = set(data)
 
-    cuda_keys = set(cuda_keys)
-    ipex_keys = set(ipex_keys)
-    xpu_keys = set(xpu_keys)
     ipex_and_xpu_keys = ipex_keys | xpu_keys
     ipex_and_xpu_keys_in_cuda = ipex_and_xpu_keys & cuda_keys
-
-
     all_xpu_keys = ipex_keys | xpu_keys | onednn_keys
+    all_xpu_keys_in_cuda = all_xpu_keys & cuda_keys
 
     with open('README.txt', 'w') as f:
-        print('Number of cuda operators:', len(cuda_keys), file=f)
-        print('Number of ipex operators:', len(ipex_keys), file=f)
-        print('Number of xpu-ops operators:', len(xpu_keys), file=f)
-        print('Total number of operators for xpu-ops + ipex:', len(ipex_and_xpu_keys_in_cuda), file=f)
-        print('Total number of operators for xpu-ops + ipex + onednn:', len(all_xpu_keys), file=f)
+        print('Description:\nThis is a temporary project for automating the progress report of torch-xpu-ops repo [https://github.com/intel/torch-xpu-ops].', file=f)
+        print('Using command `python report.py $FOLDER_TO_PYTORCH` will generate report file named README.txt\n', file=f)
+
+        print('Number of cuda operators (with cudnn):', len(cuda_keys), file=f)
+        print('Number of ipex operators (without onednn):', len(ipex_keys), file=f)
+        print('Number of xpu-ops operators (without onednn):', len(xpu_keys), file=f)
+        print('Number of onednn operators:', len(onednn_keys), file=f)
+
+        print('Total number of operators for xpu-ops+ipex (do intersection with cuda):', len(ipex_and_xpu_keys_in_cuda), file=f)
+        print('Total number of operators for xpu-ops+ipex+onednn (do intersection with cuda):', len(all_xpu_keys_in_cuda), file=f)
         print('', file=f)
+
         print('Ratio: xpu-ops / cuda:', len(xpu_keys) / len(cuda_keys), file=f)
         print('Ratio: ipex / cuda:', len(ipex_keys) / len(cuda_keys), file=f)
-        print('Ratio: xpu-ops + ipex / cuda:', len(ipex_and_xpu_keys_in_cuda) / len(cuda_keys), file=f)
-        print('Ratio: xpu-ops + ipex + onednn / cuda:', len(all_xpu_keys) / len(cuda_keys), file=f)
+        print('Ratio: xpu-ops+ipex (do intersection with cuda) / cuda:', len(ipex_and_xpu_keys_in_cuda) / len(cuda_keys), file=f)
+        print('Ratio: xpu-ops+ipex+onednn (do intersection with cuda) / cuda:', len(all_xpu_keys_in_cuda) / len(cuda_keys), file=f)
