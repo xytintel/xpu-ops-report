bde339ab835ef56a44c74be3045491504eeb941c
Yutao Xu <yutao.xu@intel.com>
2024-10-10 16:23:32 +0800

    Update report.py

--- report.py
+++ report.py
@@ -51,25 +51,20 @@ if __name__ == '__main__':
         data = data['supported']
     ipex_keys = set(data)
 
-    ipex_and_xpu_keys = ipex_keys | xpu_keys
-    ipex_and_xpu_keys_in_cuda = ipex_and_xpu_keys & cuda_keys
     all_xpu_keys = ipex_keys | xpu_keys | onednn_keys
     all_xpu_keys_in_cuda = all_xpu_keys & cuda_keys
+    xpu_onednn_keys = xpu_keys | onednn_keys
+    xpu_onednn_keys_in_cuda = xpu_onednn_keys & cuda_keys
 
     with open('README.txt', 'w') as f:
         print('Description:\nThis is a temporary project for automating the progress report of torch-xpu-ops repo [https://github.com/intel/torch-xpu-ops].', file=f)
         print('Using command `python report.py $FOLDER_TO_PYTORCH` will generate report file named README.txt\n', file=f)
 
         print('Number of cuda operators (with cudnn):', len(cuda_keys), file=f)
-        print('Number of ipex operators (without onednn):', len(ipex_keys), file=f)
+        print('Number of ipex operators (with onednn):', len(ipex_keys), file=f)
         print('Number of xpu-ops operators (without onednn):', len(xpu_keys), file=f)
         print('Number of onednn operators:', len(onednn_keys), file=f)
-
-        print('Total number of operators for xpu-ops+ipex (do intersection with cuda):', len(ipex_and_xpu_keys_in_cuda), file=f)
         print('Total number of operators for xpu-ops+ipex+onednn (do intersection with cuda):', len(all_xpu_keys_in_cuda), file=f)
         print('', file=f)
 
-        print('Ratio: xpu-ops / cuda:', len(xpu_keys) / len(cuda_keys), file=f)
-        print('Ratio: ipex / cuda:', len(ipex_keys) / len(cuda_keys), file=f)
-        print('Ratio: xpu-ops+ipex (do intersection with cuda) / cuda:', len(ipex_and_xpu_keys_in_cuda) / len(cuda_keys), file=f)
-        print('Ratio: xpu-ops+ipex+onednn (do intersection with cuda) / cuda:', len(all_xpu_keys_in_cuda) / len(cuda_keys), file=f)
+        print('Ratio: xpu-ops+onednn / cuda:', len(xpu_onednn_keys_in_cuda) / len(cuda_keys), file=f)
